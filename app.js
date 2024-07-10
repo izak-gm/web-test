edbc89e3e784f6c4d2c5e90d56980a2b8a53cb85
izak-gm <138903932+izak-gm@users.noreply.github.com>
2024-07-10 13:54:36 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-// menu toggle
-$(document).ready()
+const menu = document.querySelector("#mobile-menu");
+const menu__links = document.querySelector(".header__menu");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menu__links.classList.toggle("active");
+});
